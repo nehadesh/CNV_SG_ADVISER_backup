238520130d73aa379e6c7bda0e6a0d6896147668
Neha Deshpande <neha.n.deshpande@gmail.com>
2014-04-23 11:16:58 -0700

    test

---
+++ src/org/scripps/samples/TwoDArrays.java
@@ -0,0 +1,10 @@
+package org.scripps.samples;
+
+public class TwoDArrays
+{
+	public static void main(String[] args)
+	{
+		System.out.println("this is a test");
+		System.out.println("this is a second test");
+	}
+}
